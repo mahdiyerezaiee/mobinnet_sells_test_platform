22e14def810a7aad100fb74eab01f46d7348a02a
mehrshad darvish <mahdiyerezaiee88@gmail.com>
2023-04-24 18:09:40 +0330

    [fix] error handelling

--- src/Services/httpService.js
+++ src/Services/httpService.js
@@ -7,40 +7,14 @@ axios.defaults.headers.get["x-api-key"] = "PMAK-61325d70088f41003cdecd8a-dcb8940
 axios.interceptors.response.use(
 
     resp => resp, async error => {
-        if (error.response.status === 500) {
-
-            console.log(error);
-            if (error.response.data.error.errorCode >= 400 && error.response.data.error.errorCode <= 499) {
-
-                toast.warning(error.response.data.error.message, {
-                    position: "top-right",
-                    closeOnClick: true
-                });
-            }
-            else if(error.response.data.error.errorCode===500 && error.response.data.error.message!=="") {
-
-                toast.error(error.response.data.error.message, {
-                    position: "top-right",
-                    closeOnClick: true
-                });
-            }
-            else{
-                toast.error('سرور پاسخگو نیست', {
-                    position: "top-right",
-                    closeOnClick: true
-                });
-            }
-
-
+        if ( error.response.status >= 300 ) {
+            toast.error('سرور پاسخگو نیست', {
+                position: "top-right",
+                closeOnClick: true
+            });
         }
-
-
         return Promise.reject(error);
     });
-
-
-
 export default {
     get: axios.get,
-
 };
